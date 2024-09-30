44b85f8519481be664669712149cda844123accc
totojack <tomjack.dev@gmail.com>
2024-09-30 17:21:21 -0300

    Refactorización de las peticiones a movies

--- clase4/app.js
+++ clase4/app.js
@@ -14,29 +14,7 @@ const app = express(); // Creates an express application
 app.use(json()); // Middleware that parses the request body and stores it in req.body
 app.disable('x-powered-by'); // Disables the x-powered-by header
 
-
-// Array with specified origin (CORS) that allows the request from them. Use '*' if want to allow all origins ; Allows all origins to access the resource on the server (CORS)
-// const ACCEPTED_ORIGINS = [
-//     'http://localhost:8080',
-//     'http://localhost:1234',
-//     'https://movies.com'
-// ];
-
-// app.get('/', (req, res) => res.send('Bienvenido a la API de películas'));
-
-app.use('/movies', moviesRouter); // Use the moviesRouter for the /movies path of the server
-
-// // CORS Pre-flight request --> OPTIONS request to handle the /movies path of the server --> used to describe the communication options for the target resource
-// app.options('/movies/:id', (req, res) => {
-//     const origin = req.header('origin') // get the origin header from the request
-
-//     // Check if the origin is in the accepted origins or if the origin is not present
-//     if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
-//         res.header('Access-Control-Allow-Origin', origin); // Allow the request from the specified origin (CORS)
-//         res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, PATCH'); // Allow the specified methods
-//     }
-//     res.send(); // Send an empty response
-// });
+app.use('/movies', moviesRouter);
 
 const PORT = process.env.PORT ?? 1234; // Define the port where the server will listen
 
--- clase4/routes/movies.js
+++ clase4/routes/movies.js
@@ -2,10 +2,9 @@ import { Router } from 'express';
 import { randomUUID } from 'node:crypto'; // Import the crypto library --> used to generate random UUIDs
 import { validateMovie, validatePartialMovie } from '../schemas/movies.js'; // Importing functions to validate the movie object and the partial movie object
 import { readJSON } from '../utils.js';
+import { corsMiddleware } from '../middlewares/cors.js';
 
 const movies = readJSON('./movies.json'); // Read the movies.json file and parse it to a JSON object
-
-export const moviesRouter = Router();
 
 const ACCEPTED_ORIGINS = [
     'http://localhost:8080',
@@ -13,33 +12,10 @@ const ACCEPTED_ORIGINS = [
     'https://movies.com'
 ];
 
-// Función para manejar CORS de forma manual
-const handleCORS = (req, res) => {
-    const origin = req.header('origin'); // Obtener el header "origin" de la solicitud
-    if (ACCEPTED_ORIGINS.includes(origin) || !origin) { // Verificar si el origen es aceptado
-        res.header('Access-Control-Allow-Origin', origin); // Permitir la solicitud desde ese origen
-        res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, PATCH'); // Métodos permitidos
-        res.header('Access-Control-Allow-Headers', 'Content-Type'); // Encabezados permitidos
-    }
-};
-
-moviesRouter.get('/:id', (req, res) => {
-    handleCORS(req, res);
-
-    const { id } = req.params; // Destructure the id parameter from the request params
-    const movie = movies.find(movie => movie.id === id); // Find the movie with the specified id
-    if (movie) return res.json(movie) // Return the movie if it exists
-
-    res.status(404).json({ message: 'Movie not found' }); // Return a 404 status code if the movie does not exist
-});
+export const moviesRouter = Router();
 
 moviesRouter.get('/', (req, res) => {
-    // const origin = req.header('origin') // get the origin header from the request
-    // if (ACCEPTED_ORIGINS.includes(origin) || !origin) { // Check if the origin is in the accepted origins or if the origin is not present
-    //     res.header('Access-Control-Allow-Origin', origin); // Allow the request from the specified origin (CORS).
-    // }
-    handleCORS(req, res);
-
+    corsMiddleware(req, res); // Call the corsMiddleware function to handle the CORS
     const { genre } = req.query; // Destructure the genre query parameter from the request query
     if (genre) {
         const filteredMovies = movies.filter( // Filter the movies array based on the genre query parameter
@@ -51,9 +27,14 @@ moviesRouter.get('/', (req, res) => {
     res.json(movies); // Return all the movies
 });
 
-moviesRouter.post('/', (req, res) => {
-    handleCORS(req, res);
+moviesRouter.get('/:id', (req, res) => {
+    const { id } = req.params; // Destructure the id parameter from the request params
+    const movie = movies.find(movie => movie.id === id); // Find the movie with the specified id
+    if (movie) return res.json(movie) // Return the movie if it exists
+    res.status(404).json({ message: 'Movie not found' }); // Return a 404 status code if the movie does not exist
+});
 
+moviesRouter.post('/', (req, res) => {
     const result = validateMovie(req.body); // Validate the movie object from the request body
 
     // If the validation fails, return a 400 status code with the error message
@@ -71,8 +52,6 @@ moviesRouter.post('/', (req, res) => {
 });
 
 moviesRouter.patch('/:id', (req, res) => {
-    handleCORS(req, res);
-
     const result = validatePartialMovie(req.body); // Validate the partial movie object from the request body
 
     // If the validation fails, it return a 400 status code with the error message
@@ -97,15 +76,8 @@ moviesRouter.patch('/:id', (req, res) => {
 });
 
 moviesRouter.delete('/:id', (req, res) => {
-    handleCORS(req, res);
-
-    // const origin = req.header('origin') // get the origin header from the request
-    // Check if the origin is in the accepted origins or if the origin is not present
-    // if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
-    //     res.header('Access-Control-Allow-Origin', origin); // Allow the request from the specified origin (CORS)
-    // }
-
-    const id = req.params; // Destructure the id parameter from the request params
+    corsMiddleware(req, res); // Call the corsMiddleware function to handle the CORS
+    const { id } = req.params; // Destructure the id parameter from the request params
     const movieIndex = movies.findIndex(movie => movie.id === id); // Find the index of the movie with the specified id
 
     if (movieIndex === -1) return res.status(404).json({ message: "404 Not Found" }); // Return a 404 status code if the movie does not exist
@@ -114,15 +86,19 @@ moviesRouter.delete('/:id', (req, res) => {
     return res.json({ message: "Movie deleted" }); // Return a JSON response with a message
 });
 
-// CORS Pre-flight request --> OPTIONS request to handle the /movies path of the server --> used to describe the communication options for the target resource
-moviesRouter.options('/movies/:id', (req, res) => {
-    handleCORS(req, res);
-    // const origin = req.header('origin') // get the origin header from the request
+moviesRouter.options('/:id', (req, res) => {
+    const origin = req.header('origin') // get the origin header from the request
 
-    // // Check if the origin is in the accepted origins or if the origin is not present
-    // if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
-    //     res.header('Access-Control-Allow-Origin', origin); // Allow the request from the specified origin (CORS)
-    //     res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, PATCH'); // Allow the specified methods
-    // }
-    res.send(); // Send an empty response
+    // Check if the origin is in the accepted origins or if the origin is not present
+    if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+        res.header('Access-Control-Allow-Origin', origin); // Allow the request from the specified origin (CORS)
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE'); // Allow the specified methods
+    }
+    res.send(200); // Send an empty response
 });
+
+// // CORS Pre-flight request --> OPTIONS request to handle the /movies path of the server --> used to describe the communication options for the target resource
+// moviesRouter.options('/movies/:id', (req, res) => {
+//     handleCORS(req, res);
+//     res.send(); // Send an empty response
+// });
