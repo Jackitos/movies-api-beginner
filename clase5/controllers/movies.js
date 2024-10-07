e7da400192a289a7c5c3622ca6c5b01d5661b1d9
totojack <tomjack.dev@gmail.com>
2024-10-07 15:51:20 -0300

    Class 5 - using Dependency Injection

--- clase5/controllers/movies.js
+++ clase5/controllers/movies.js
@@ -1,26 +1,27 @@
-// import { MovieModel } from "../models/local-file-system/movie.js";
-import { MovieModel } from "../models/mysql/movie.js";
 import { corsMiddleware } from "../middlewares/cors.js";
 import { validateMovie, validatePartialMovie } from "../schemas/movies.js";
 
 export class MovieController {
-    static async getAll(req, res) {
+    constructor({ movieModel }) {
+        this.movieModel = movieModel;
+    }
+    getAll = async (req, res) => {
         corsMiddleware(req, res); // Call the corsMiddleware function to handle the CORS
         const { genre } = req.query; // Destructure the genre query parameter from the request query
-        const movies = await MovieModel.getAll({ genre });
+        const movies = await this.movieModel.getAll({ genre });
 
         // Qué es lo que renderiza este método?
         res.json(movies); // Return all the movies
     }
 
-    static async getById(req, res) {
+    getById = async (req, res) => {
         const { id } = req.params; // Destructure the id parameter from the request params
-        const movie = await MovieModel.getById({ id });
+        const movie = await this.movieModel.getById({ id });
         if (movie) return res.json(movie) // Return the movie if it exists
         res.status(404).json({ message: 'Movie not found' }); // Return a 404 status code if the movie does not exist
     }
 
-    static async create(req, res) {
+    create = async (req, res) => {
         const result = validateMovie(req.body); // Validate the movie object from the request body
 
         // If the validation fails, return a 400 status code with the error message
@@ -29,34 +30,34 @@ export class MovieController {
         }
 
         // If the validation passes, create a new movie object with a random UUID and the movie object from the request body
-        const newMovie = await MovieModel.create({ input: result.data });
+        const newMovie = await this.movieModel.create({ input: result.data });
         res.status(201).json(newMovie); // Return a 201 status code with the new movie object
     }
 
-    static async delete(req, res) {
+    delete = async (req, res) => {
         corsMiddleware(req, res); // Call the corsMiddleware function to handle the CORS
         const { id } = req.params; // Destructure the id parameter from the request params
 
-        const result = await MovieModel.delete(({ id }))
+        const result = await this.movieModel.delete(({ id }))
         if (result === false) return res.status(404).json({ message: "Movie Not Found" }); // Return a 404 status code if the movie does not exist
 
         return res.json({ message: "Movie deleted" }); // Return a JSON response with a message
     }
 
-    static async update(req, res) {
+    update = async (req, res) => {
         const result = validatePartialMovie(req.body); // Validate the partial movie object from the request body
 
         // If the validation fails, it return a 400 status code with the error message
         if (!result.success) {
-        return res.status(400).json({ error: JSON.parse(result.error.message) });
+            return res.status(400).json({ error: JSON.parse(result.error.message) });
         }
-    
+
         const { id } = req.params; // Destructure the id parameter from the request params
-    
-        const updatedMovie = await MovieModel.update({ id, input: result.data });
+
+        const updatedMovie = await this.movieModel.update({ id, input: result.data });
 
         if (!updatedMovie) return res.status(404).json({ message: "Movie Not Found" }); // Return a 404 status code if the movie does not exist
-    
+
         return res.json(updatedMovie); // return the updated movie object
     }
 }
--- clase5/routes/movies.js
+++ clase5/routes/movies.js
@@ -1,30 +1,36 @@
 import { Router } from 'express';
 import { MovieController } from '../controllers/movies.js';
 
-const ACCEPTED_ORIGINS = [
-    'http://localhost:8080',
-    'http://localhost:1234',
-    'https://movies.com'
-];
+export const createMovieRouter = ({ movieModel }) => {
+    const moviesRouter = Router();
 
-export const moviesRouter = Router();
+    const movieController = new MovieController({ movieModel });
 
-moviesRouter.get('/', MovieController.getAll);
-moviesRouter.get('/:id', MovieController.getById);
-moviesRouter.post('/', MovieController.create);
-moviesRouter.delete('/:id', MovieController.delete);
-moviesRouter.patch('/:id', MovieController.update);
+    const ACCEPTED_ORIGINS = [
+        'http://localhost:8080',
+        'http://localhost:1234',
+        'https://movies.com'
+    ];
 
-moviesRouter.options('/:id', (req, res) => {
-    const origin = req.header('origin') // get the origin header from the request
+    moviesRouter.get('/', movieController.getAll);
+    moviesRouter.get('/:id', movieController.getById);
+    moviesRouter.post('/', movieController.create);
+    moviesRouter.delete('/:id', movieController.delete);
+    moviesRouter.patch('/:id', movieController.update);
+    
+    moviesRouter.options('/:id', (req, res) => {
+        const origin = req.header('origin') // get the origin header from the request
+    
+        // Check if the origin is in the accepted origins or if the origin is not present
+        if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+            res.header('Access-Control-Allow-Origin', origin); // Allow the request from the specified origin (CORS)
+            res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE'); // Allow the specified methods
+        }
+        res.send(200); // Send an empty response
+    });
 
-    // Check if the origin is in the accepted origins or if the origin is not present
-    if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
-        res.header('Access-Control-Allow-Origin', origin); // Allow the request from the specified origin (CORS)
-        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE'); // Allow the specified methods
-    }
-    res.send(200); // Send an empty response
-});
+    return moviesRouter;
+}
 
 // // CORS Pre-flight request --> OPTIONS request to handle the /movies path of the server --> used to describe the communication options for the target resource
 // moviesRouter.options('/movies/:id', (req, res) => {
